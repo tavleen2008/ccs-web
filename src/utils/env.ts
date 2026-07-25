29b327f766d8c0e2a7674d777b09c1e3561f6eff
vaishnaviinit <vaishnavipixy@gmail.com>
2026-07-25 19:20:16 +0530

    fixed build errors

--- src/utils/env.ts
+++
@@ -1,4 +0,0 @@
-export type TEnvironment = "production" | "staging" | "latest" | "development";
-export const APP_ENV = (process.env.REACT_APP_APP_ENV ??
-  "development") as TEnvironment;
-export const IS_PRODUCTION = APP_ENV === "production";
